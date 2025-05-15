1033805fe479801f676840d92d1315bd35a2744f
Aaron HeRo <aaron.hero1103@gmail.com>
2025-05-15 11:54:12 -0600

    Se agrego carpeta de archivos originales

---
+++ Sobrecarga-Aaron-Hernandez/Codigos originales/contador_original.cpp
@@ -0,0 +1,15 @@
+/*
+// contador_inc_bug.cpp
+#include <iostream>
+using namespace std;
+
+class Contador {
+    int valor;
+public:
+    Contador(int v) : valor(v) {}
+    Contador operator++(int) {
+        valor++;
+        return *this;  // BUG: ¿qué comportamiento falla?
+    }
+};
+*/
