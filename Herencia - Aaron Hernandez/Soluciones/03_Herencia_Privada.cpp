9deeff3ff9a54e20e17a1ef1b1a0c6a31e117031
Aaron HeRo <aaron.hero1103@gmail.com>
2025-05-19 12:44:26 -0600

    Se modifico el ejercicio 3

--- Herencia - Aaron Hernandez/Soluciones/03_Herencia_Privada.cpp
+++ Herencia - Aaron Hernandez/Soluciones/03_Herencia_Privada.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Vehiculo {
 public:
+    string marca;
+    string tipo;
+
+    Vehiculo(const string& m, const string& t): marca(m), tipo(t) { }
+
     void encender() {
-        cout << "Vehículo encendido" << endl;
+        cout << tipo << " se ha encendido" << endl;
+    }
+
+    void mostrarDatos() {
+        cout << "Marca: " << marca << endl;
+        cout << "Tipo: " << tipo << endl;
     }
 };
 
 class Coche : private Vehiculo {
-    public:
-        void encender() {
-            Vehiculo::encender();
-        }
+public:
+    Coche(const string& m) : Vehiculo(m, "Carro") { }
+    
+    void encender() {
+        Vehiculo::encender();
+    }
+
+    void mostrarDatos() {
+        Vehiculo::mostrarDatos();
+    }
 };
 
 int main() {
-    Coche miCoche;
+    Coche miCoche("Ford");
     miCoche.encender(); // ¿Por qué falla?
+    miCoche.mostrarDatos(); // ¿Por qué falla?
     return 0;
 }
