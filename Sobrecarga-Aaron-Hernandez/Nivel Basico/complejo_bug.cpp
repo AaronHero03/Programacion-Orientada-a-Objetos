106bddae59ebfdfddcb759a5b7680491e3111621
Aaron HeRo <aaron.hero1103@gmail.com>
2025-05-19 11:34:12 -0600

    Se separo en folders

--- Sobrecarga-Aaron-Hernandez/Nivel Basico/complejo_bug.cpp
+++ Sobrecarga-Aaron-Hernandez/Nivel Basico/complejo_bug.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Complejo {
 public:
     double re, im;
-    Complejo(double r, double i) : re(r), im(i) {}
+    Complejo(double r, double i);
     // BUG: firma incorrecta y falta const
     Complejo operator+(const Complejo& otro) {
         return Complejo(re + otro.im, im + otro.re);
